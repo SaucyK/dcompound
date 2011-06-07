8405de4eaa97cde603fa44fb797d85b550c67800
Dave K <dave.kerr@gmail.com>
2011-06-07 21:21:09 +0100

    oops

--- drug compound.app/Contents/Resources/assets/default/images/drug compound.app/Contents/Resources/default/images/drug compound.app/Contents/Resources/task.rb
+++
@@ -1,5 +0,0 @@
-class Task
-  attr_accessor :title, :icon, :action, :target, :assignee, :progress
-  
-  
-end
--- drug compound.app/Contents/Resources/assets/default/images/drug compound.app/Contents/Resources/images/drug compound.app/Contents/Resources/images/drug compound.app/Contents/Resources/lib/resource.rb
+++
@@ -1,9 +0,0 @@
-class Resource < GameObject
-  attr_accessor :type, :value, :weight, :block
-  
-  
-  def add_to_block(block)
-    @block = block
-    @block.add_fringe(self)
-  end
-end
